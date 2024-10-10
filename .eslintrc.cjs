84b86050391d74a5f5d18212384db3953c18d1e5
kitety <aizaizuori@foxmail.com>
2024-10-10 19:01:32 +0800

    init basic config

---
+++ .eslintrc.cjs
@@ -0,0 +1,40 @@
+module.exports = {
+  extends: ['expo', 'prettier'],
+  plugins: ['prettier', 'simple-import-sort', 'import'],
+  parser: '@typescript-eslint/parser',
+  ignorePatterns: ['.eslintrc.cjs', 'tailwind.config.js'],
+  parserOptions: {
+    project: './tsconfig.json',
+    ecmaVersion: 2021,
+    sourceType: 'module',
+  },
+  rules: {
+    'simple-import-sort/imports': 'off',
+    'simple-import-sort/exports': 'off',
+    'import/first': 'error',
+    'import/newline-after-import': 'error',
+    'import/no-duplicates': 'error',
+    'prettier/prettier': 'error',
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: 'interface',
+        format: ['PascalCase'],
+        custom: {
+          regex: '^I[A-Z]',
+          match: true,
+        },
+      },
+    ],
+    'react/jsx-sort-props': [
+      'error',
+      {
+        callbacksLast: true,
+        shorthandFirst: true,
+        noSortAlphabetically: false,
+        ignoreCase: true,
+        multiline: 'last',
+      },
+    ],
+  },
+};
